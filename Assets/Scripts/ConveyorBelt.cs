90437d018f65b5f3b009b56bd3ce29c05de23f08
Zion Perez <zionperez@gmail.com>
2019-04-27 13:08:48 +0900

    First commit.

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+
+    public GameObject belt;
+    public Transform endpoint;
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // crate current position: other.transform.position
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
